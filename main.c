7b21c55b2ade237f945c08a74ddd4fd83747d8c4
Melissa Tully <91146797+Mel-Tully@users.noreply.github.com>
2022-09-14 12:51:16 -0500

    input error checking

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "input_error.h"
+
 int main(int argc, char *argv[])
 {
-    
+    if (argc != 1)
+    {
+        exit(INCORRECT_NUMBER_OF_COMMAND_LINE_ARGUMENTS);
+    }
+
+    FILE fp = fopen(argv[1], )
 }
