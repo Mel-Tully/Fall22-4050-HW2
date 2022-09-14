c4c70cfaec7fdabcbe71b0dc33ba97d651bb938f
Zoe <82968887+zyshff@users.noreply.github.com>
2022-09-14 12:44:52 -0500

    Create MOM.c

---
+++ MOM.c
@@ -0,0 +1,28 @@
+#include stdio.h
+#include stdlib.h
+#include "MOM.h"
+
+typedef struct listElem list;
+
+struct listElem
+{
+    list * next;
+    int length;
+}
+
+//need to update method types
+void Partition(list * A, int p)
+{
+
+}
+
+//need to update method types
+void MOMSelect(list * A, int k)
+{
+
+
+}
+
+
+
+
