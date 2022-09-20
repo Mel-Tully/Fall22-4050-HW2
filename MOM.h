dc4e9ac55c2476476d033a13bf7af89d65b9c9f5
Zoe <82968887+zyshff@users.noreply.github.com>
2022-09-20 15:43:19 -0500

    Created MOM.h header file

---
+++ MOM.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void Merge(int ** arr, int l, int mid, int r);
+void Partition(int * array, int p);
+void MOMSelect(int * array, int k, int l);
+
+void printArray(int * array, int length);
+
+void MergeSort(int * array, int l, int r, int length); 
+void Merge(int ** arr, int l, int mid, int r); 
