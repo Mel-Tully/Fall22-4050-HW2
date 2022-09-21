0204961496e09a9ceec7e6e3ba916955ba10e542
Zoe <82968887+zyshff@users.noreply.github.com>
2022-09-20 22:40:35 -0500

    Added linked_list.c to reduce errors, finished writing methods in MOM.c
    Basically wrote something for every method, didn't get the chance to test whether MOMHelper and MOMSelect actually work as intended. Pretty sure FindMedian works, but it could also use more testing

--- linked_list.h
+++ linked_list.h
@@ -32,37 +32,7 @@ void free_linked_list(Linked_list_t * list);
 /// @param node_value a non-negative integer.
 void insert_at_head(Linked_list_t * list, int node_value);
 
-
-/* Function Definitions */
-
-Linked_list_t * new_linked_list()
-{
-    Linked_list_t * input = malloc(sizeof(Linked_list_t));
-    input->head = NULL;
-    input->size = 0;
-    return input;
-}
-
-void free_linked_list(Linked_list_t * list)
-{
-    Node_t * new_head;
-
-    while (list->size > 0)
-    {
-        new_head = list->head->next;
-        free(list->head);
-        list->head = new_head;
-        list->size = list->size - 1;
-    }
-    
-    free(list);
-}
-
-void insert_at_head(Linked_list_t * list, int node_value)
-{
-    Node_t * new_node = malloc(sizeof(Node_t));
-    new_node->next = list->head;
-    new_node->number = node_value;
-    list->head = new_node;
-    list->size = list->size + 1;
-}
+/// @brief Turns a linked list of integers into an integer array
+/// @param input A nonzero linked list of integers
+/// @return An integer array
+int * list_to_array(Linked_list_t * input);
